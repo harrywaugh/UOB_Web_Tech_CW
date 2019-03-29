26c63ebee8b0c189611bac22187a1e2d84d2eb98
Finn <finn@hobsonmail.com>
2019-03-29 12:20:48 +0000

    Displaying Username on Homepage

--- site/express.js
+++ site/express.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+var bodyParser = require("body-parser");
 const port = 8080
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.set('view engine', 'pug')
 app.set('views', './views')
@@ -16,9 +20,9 @@ app.get('/login', function (req, res) {
 })
 
 app.post('/', function (req, res) {
-  response = {
-    username:req.body.username,
-    password:req.body.password
-  };
-  console.log(response)
+  var username = req.body.username;
+  var password = req.body.password;
+  console.log(username)
+  console.log(password)
+  res.render('pages/home', { title: 'Home Page', welcome_name: username })
 })
