c11fdc0aaf4e10c2eacea0aa1e1c402d3545ef78
Harry Waugh <hw16471@my.bristol.ac.uk>
2019-04-14 16:09:22 +0100

    Node server checks if two passwords are the same, prints err msg

---
+++ site/server.js
@@ -0,0 +1,72 @@
+/////////////////////////////////
+// Node server setup
+/////////////////////////////////
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser");
+const sqlite3 = require('sqlite3').verbose();
+const port = 8080
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+
+/////////////////////////////////
+// Set server to listen on port
+/////////////////////////////////
+app.listen(port, () => console.log(`Server started, running on ${port}.`))
+
+
+/////////////////////////////////
+// Map GET requests
+/////////////////////////////////
+app.get('/',         function (req, res) { res.render('pages/home', { welcome_name: 'there' }) })
+app.get('/login',    function (req, res) { res.render('pages/login') })
+app.get('/forum',    function (req, res) { res.render('pages/forum') })
+app.get('/tutorial', function (req, res) { res.render('pages/tutorial') })
+app.get('/new_user', function (req, res) { res.render('pages/new_user') })
+
+
+/////////////////////////////////
+// Map POST requests
+/////////////////////////////////
+app.post('/login', function (req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  console.log("\nUser logged in with the following credentials\nUsername = " + username);
+  console.log("Password = " + password);
+  res.render('pages/home', { welcome_name: username })
+})
+app.post('/new_user', function (req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+
+  if (password === password2)  {
+    console.log("\nUser created an account with the following credentials\nUsername = " + username);
+    console.log("Password = " + password);
+    console.log("Password2 = " + password2);
+    res.render('pages/home', { welcome_name: username })
+  } else {
+    res.render('pages/new_user', { error_msg: "Passwords dont match" })
+  }
+})
+
+// open database in memory
+let db = new sqlite3.Database('./db/users.db', (err) => {
+  if (err) {
+    console.log('Error: Opening Database\n');
+    return console.error(err.message);
+  }
+  console.log('Connection opened to database.');
+});
+
+// close the database connection
+db.close((err) => {
+  if (err) {
+    console.log('Error: Closing Database\n');
+    return console.error(err.message);
+  }
+  console.log('Connection closed to database.');
+});
