db390fe1da0c90a28ec9d0cd7401a55a52b9a526
Harry Waugh <hw16471@my.bristol.ac.uk>
2019-04-14 19:00:09 +0100

    Create account works, logging in works, rejects bad password

--- site/server.js
+++ site/server.js
@@ -1,3 +1,4 @@
+"use strict";
 /////////////////////////////////
 // Node server setup
 /////////////////////////////////
@@ -34,39 +35,60 @@ app.get('/new_user', function (req, res) { res.render('pages/new_user') })
 app.post('/login', function (req, res) {
   var username = req.body.username;
   var password = req.body.password;
-  console.log("\nUser logged in with the following credentials\nUsername = " + username);
-  console.log("Password = " + password);
-  res.render('pages/home', { welcome_name: username })
+  check_login(username, password, req, res);
 })
 app.post('/new_user', function (req, res) {
   var username = req.body.username;
   var password = req.body.password;
   var password2 = req.body.password2;
-
-  if (password === password2)  {
-    console.log("\nUser created an account with the following credentials\nUsername = " + username);
-    console.log("Password = " + password);
-    console.log("Password2 = " + password2);
-    res.render('pages/home', { welcome_name: username })
-  } else {
-    res.render('pages/new_user', { error_msg: "Passwords dont match" })
-  }
+  create_user(username, password, password2, req, res);
 })
 
-// open database in memory
+/////////////////////////////////
+// Open database connection
+/////////////////////////////////
 let db = new sqlite3.Database('./db/users.db', (err) => {
   if (err) {
     console.log('Error: Opening Database\n');
     return console.error(err.message);
   }
   console.log('Connection opened to database.');
+  db.exec(db_schema);
+  insert("hw16471", "pass");
 });
 
-// close the database connection
-db.close((err) => {
-  if (err) {
-    console.log('Error: Closing Database\n');
-    return console.error(err.message);
+/////////////////////////////////
+// Database queries
+/////////////////////////////////
+const db_schema = "CREATE TABLE IF NOT EXISTS Accounts ('username', 'password');"
+let insert = function(username, password){
+  db.exec("INSERT INTO Accounts (username, password) VALUES ('"+username+"', '"+password+"');");
+}
+
+function check_login(username, password, req, res){
+  db.all("select * from Accounts;" , (err, rows) => {
+  if (err) throw err; 
+  for (var i = 0; i < rows.length; i++) {
+    if (rows[i]['username'] === username && rows[i]['password'] === password)  {
+      console.log("\nUser logged in with the following credentials\nUsername = " + username);
+      console.log("Password = " + password);
+      res.render('pages/home', { welcome_name: username })
+      return;
+    }
   }
-  console.log('Connection closed to database.');
-});
+  res.render('pages/login', { error_msg: "Invalid Login Details" })
+  });
+}
+
+function create_user(username, password, password2, req, res){
+  if (password === password2)  {
+    console.log("\nUser created an account with the following credentials\nUsername = " + username);
+    console.log("Password = " + password);
+    console.log("Password2 = " + password2);
+    insert(username, password);
+
+    res.render('pages/home', { welcome_name: username })
+  } else {
+    res.render('pages/new_user', { error_msg: "Passwords dont match" })
+  }
+}
