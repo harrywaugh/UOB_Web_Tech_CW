24bcf1385d4a8ec20b4b97e50f0ef5842ac13c0f
Harry Waugh <hw16471@my.bristol.ac.uk>
2019-04-14 14:57:53 +0100

    Cleaned up express.js, added template links to pages

--- site/express.js
+++ site/express.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
-const sqlite3 = require('sqlite3').verbose();
+// const sqlite3 = require('sqlite3').verbose();
 const port = 8080
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,35 +10,32 @@ app.use(bodyParser.json());
 app.set('view engine', 'pug')
 app.set('views', './views')
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Server started, running on ${port}.`))
 
-app.get('/', function (req, res) {
-  res.render('pages/home', { title: 'Home Page', welcome_name: 'there' })
-})
-
-app.get('/login', function (req, res) {
-  res.render('pages/login', { title: 'Login Page' })
-})
+app.get('/',         function (req, res) { res.render('pages/home', { welcome_name: 'there' }) })
+app.get('/login',    function (req, res) { res.render('pages/login') })
+app.get('/forum',    function (req, res) { res.render('pages/forum') })
+app.get('/tutorial', function (req, res) { res.render('pages/tutorial') })
 
 app.post('/', function (req, res) {
   var username = req.body.username;
   var password = req.body.password;
-  console.log("Username = " + username + "\nPassword = " + password);
+  console.log("\nUser logged in with the following credentials\nUsername = " + username + "\nPassword = " + password);
   res.render('pages/home', { title: 'Home Page', welcome_name: username })
 })
 
-// open database in memory
-let db = new sqlite3.Database('./db/users.db', (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Connected to the in-memory SQlite database.');
-});
+// // open database in memory
+// let db = new sqlite3.Database('./db/users.db', (err) => {
+//   if (err) {
+//     return console.error(err.message);
+//   }
+//   console.log('Connected to the in-memory SQlite database.');
+// });
 
-// close the database connection
-db.close((err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Close the database connection.');
-});
+// // close the database connection
+// db.close((err) => {
+//   if (err) {
+//     return console.error(err.message);
+//   }
+//   console.log('Close the database connection.');
+// });
