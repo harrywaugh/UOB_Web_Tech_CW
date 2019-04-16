156ddebdf30ea246103ca314f573d3e3f56c6241
Harry Waugh <hw16471@my.bristol.ac.uk>
2019-04-16 18:01:44 +0100

    Fully working sessions, can log out

--- site/server.js
+++ site/server.js
@@ -1,13 +1,25 @@
 "use strict";
 
 /////////////////////////////////
-// Node Configuration
+// Constant Initialisation
 /////////////////////////////////
 const express = require('express')
+const session = require('express-session');
+const uuid = require('uuid/v4')
 const app = express()
 const bodyParser = require("body-parser");
 const sqlite3 = require('sqlite3').verbose();
 const port = 8080
+
+/////////////////////////////////
+// Node Configuration
+/////////////////////////////////
+app.use(session({ genid: (req) => {return uuid() }, 
+  secret: 'shhhh', 
+  resave: false,
+  saveUninitialized: true,
+  expires: new Date(Date.now() + (30 * 60 * 1000)) //Check this! Sessions should expire after 30 minutes
+}))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set('view engine', 'pug')
@@ -23,11 +35,11 @@ app.listen(port, () => console.log(`Server started, running on ${port}.`))
 /////////////////////////////////
 // Map GET requests
 /////////////////////////////////
-app.get('/',         function (req, res) { res.render('pages/home', { welcome_name: 'there' }) })
-app.get('/login',    function (req, res) { res.render('pages/login') })
-app.get('/forum',    function (req, res) { res.render('pages/forum') })
-app.get('/tutorial', function (req, res) { res.render('pages/tutorial') })
-app.get('/new_user', function (req, res) { res.render('pages/new_user') })
+app.get('/',         function (req, res) { existing_session('pages/home',     req, res);})
+app.get('/login',    function (req, res) { res.render('pages/login'); })
+app.get('/forum',    function (req, res) { existing_session('pages/forum',    req, res); })
+app.get('/tutorial', function (req, res) { existing_session('pages/tutorial', req, res); })
+app.get('/new_user', function (req, res) { res.render('pages/new_user'); })
 
 
 /////////////////////////////////
@@ -37,6 +49,9 @@ app.post('/login', function (req, res) {
   var username = req.body.username;
   var password = req.body.password;
   check_login(username, password, req, res);
+})
+app.post('/logout', function (req, res) {
+  logout(req, res);
 })
 app.post('/new_user', function (req, res) {
   var username = req.body.username;
@@ -55,26 +70,32 @@ let db = new sqlite3.Database('./db/users.db', (err) => {
   }
   console.log('Connection opened to database.');
   db.exec(db_schema);
-  insert("hw16471", "pass");
+  insert_entry("hw16471", "pass", "NULL");
 });
 
 
 /////////////////////////////////
 // Database queries
 /////////////////////////////////
-const db_schema = "CREATE TABLE IF NOT EXISTS Accounts ('username', 'password');"
-let insert = function(username, password){
-  db.exec("INSERT INTO Accounts (username, password) VALUES ('"+username+"', '"+password+"');");
+const db_schema = "CREATE TABLE IF NOT EXISTS Accounts (username TEXT PRIMARY KEY,password TEXT,session TEXT);"
+
+function insert_entry(username, password, sessionID){
+  db.all("select * from Accounts where username='"+username+"';" , (err, rows) => {
+    if (err) throw err;
+    if(rows.length == 0)
+      db.exec("INSERT INTO Accounts (username,password,session) VALUES ('"+username+"','"+password+"','"+sessionID+"');");
+  });
 }
 
 function check_login(username, password, req, res){
-  db.all("select * from Accounts;" , (err, rows) => {
+  db.all("select * from Accounts where username='"+username+"';" , (err, rows) => {
   if (err) throw err; 
   for (var i = 0; i < rows.length; i++) {
-    if (rows[i]['username'] === username && rows[i]['password'] === password)  {
+    if (rows[i]['password'] === password)  { //Needs to hashed
       console.log("\nUser logged in with the following credentials\nUsername = " + username);
       console.log("Password = " + password);
-      res.render('pages/home', { welcome_name: username })
+      db.exec("REPLACE INTO Accounts (username,password,session) VALUES ('"+username+"','"+password+"','"+req.sessionID+"');");
+      res.render('pages/home', { welcome_name: username, logged_in: true  });
       return;
     }
   }
@@ -82,15 +103,38 @@ function check_login(username, password, req, res){
   });
 }
 
-function create_user(username, password, password2, req, res){
+function create_user(username, password, password2, req, res){ 
   if (password === password2)  {
     console.log("\nUser created an account with the following credentials\nUsername = " + username);
     console.log("Password = " + password);
     console.log("Password2 = " + password2);
-    insert(username, password);
-
-    res.render('pages/home', { welcome_name: username })
+    insert_entry(username, password, req.sessionID);
+    res.render('pages/home', { welcome_name: username, logged_in: true  });
   } else {
     res.render('pages/new_user', { error_msg: "Passwords dont match" })
   }
 }
+
+function existing_session(view, req, res){
+  db.all("select * from Accounts where session='"+req.sessionID+"';" , (err, rows) => {
+    if (err) throw err;
+    if(rows.length > 0)  {
+      res.render(view, { welcome_name: rows[0]['username'], logged_in: true });
+    } else {
+      res.render(view, { welcome_name: 'there' });
+    }
+  });
+}
+
+function logout(req, res)  {
+  db.all("select * from Accounts where session='"+req.sessionID+"';" , (err, rows) => {
+    if (err) throw err;
+    if(rows.length > 0)  {
+      db.exec("REPLACE INTO Accounts (username,password,session) VALUES ('"+rows[0]['username']+"','"+rows[0]['password']+"','NULL');");
+    }
+    res.render('pages/home', { welcome_name: 'there'});
+
+  });
+}
+
+//Logout function
