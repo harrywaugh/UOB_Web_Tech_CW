cf0bc33b70e474e411fbb6d3f019b44eb397fd9e
Finn <finn@hobsonmail.com>
2019-04-19 04:47:58 +0100

    Initial Style Update
    Added coloured backgrounds, fonts, some text and 'download from app store' images.

---
+++ site/views/js/bulma.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function() {
+    $('.navbar-burger').click(function() {
+        $(this).toggleClass('is-active');
+        $('.navbar-menu').toggleClass('is-active');
+    });
+});
