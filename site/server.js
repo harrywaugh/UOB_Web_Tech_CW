7b7a3d1a4b7dc485b82f9b25129b9f3c138ce22a
Harry Waugh <hw16471@my.bristol.ac.uk>
2019-05-11 12:07:26 +0100

    You can now reply to forum posts

--- site/server.js
+++ site/server.js
@@ -36,9 +36,10 @@ app.listen(port, 'localhost', () => console.log(`Server started, running on ${po
 /////////////////////////////////
 // Map GET requests
 /////////////////////////////////
-app.get('/',         function (req, res) { existing_session('pages/home',     req, res, {});})
-app.get('/login',    function (req, res) { res.render('pages/login'); })
-app.get('/forum',    function (req, res) { render_forum('pages/forum',    req, res); })
+app.get('/',         function (req, res) { existing_session('pages/home',     req, res, {}); })
+app.get('/error',    function (req, res) { existing_session('pages/error',    req, res, {}); })
+app.get('/login',    function (req, res) { res.render('pages/login');                        })
+app.get('/forum',    function (req, res) { render_forum('pages/forum',        req, res);     })
 app.get('/tutorial', function (req, res) { existing_session('pages/tutorial', req, res, {}); })
 app.get('/new_user', function (req, res) { res.render('pages/new_user'); })
 
@@ -64,7 +65,11 @@ app.post('/create_post', function (req, res) {
   var post = req.body.post;
   insert_post(post, req, res);
 })
-
+app.post('/reply_to_post', function (req, res) {
+  var reply = req.body.reply;
+  var post_id = req.body.post_id;
+  insert_reply(post_id, reply, req, res);
+})
 /////////////////////////////////
 // Open database connection
 /////////////////////////////////
@@ -74,24 +79,25 @@ let db = new sqlite3.Database('./db/users.db', (err) => {
     return console.error(err.message);
   }
   console.log('Connection opened to database.');
-  db.exec(account_schema);
-  db.exec(forum_schema);
-  db.exec(reply_schema);
-  insert_user("hw16471", "pass", "NULL");
-  forum_insert_post.run(["harry's message", "hw16471"]);
-  replies_insert_reply.run([1, "harry's reply", "hw16471"]);
+  db.exec(db_schema, () => {
+    insert_user("hw16471", "pass", "NULL");
+    forum_insert_post.run(["harry's message", "hw16471"]);
+    replies_insert_reply.run([1, "harry's reply", "hw16471"]);
+  });
+
 });
 
 /////////////////////////////////
 // Database queries
 /////////////////////////////////
-const account_schema = "CREATE TABLE IF NOT EXISTS Accounts (username TEXT PRIMARY KEY,password TEXT,session TEXT);"
-const forum_schema   = "CREATE TABLE IF NOT EXISTS Forum (post_id INTEGER PRIMARY KEY AUTOINCREMENT,message TEXT,\
-                        username TEXT, FOREIGN KEY (username) REFERENCES Accounts(username));"
-const reply_schema   = "CREATE TABLE IF NOT EXISTS Replies (reply_id INTEGER PRIMARY KEY AUTOINCREMENT,\
-                        post_id INTEGER,message TEXT,username TEXT,\
-                        FOREIGN KEY (post_id) REFERENCES Forum(post_id),\
-                        FOREIGN KEY (username) REFERENCES Forum(username));"
+const db_schema =  "DROP TABLE IF EXISTS Accounts; DROP TABLE IF EXISTS Forum; DROP TABLE IF EXISTS Replies;\
+                    CREATE TABLE IF NOT EXISTS Accounts (username TEXT PRIMARY KEY,password TEXT,session TEXT);\
+                    CREATE TABLE IF NOT EXISTS Forum (post_id INTEGER PRIMARY KEY AUTOINCREMENT,message TEXT,\
+                    username TEXT, FOREIGN KEY (username) REFERENCES Accounts(username));\
+                    CREATE TABLE IF NOT EXISTS Replies (reply_id INTEGER PRIMARY KEY AUTOINCREMENT,\
+                    post_id INTEGER,message TEXT,username TEXT,\
+                    FOREIGN KEY (post_id) REFERENCES Forum(post_id),\
+                    FOREIGN KEY (username) REFERENCES Forum(username));"
 
 
 const account_select_username = db.prepare("SELECT * FROM Accounts WHERE username=?");
@@ -103,38 +109,6 @@ const forum_select            = db.prepare("SELECT * FROM Forum;");
 const replies_insert_reply    = db.prepare("INSERT INTO Replies (reply_id,post_id,message,username) VALUES (NULL,?,?,?);");
 const replies_select          = db.prepare("SELECT * FROM Replies;");
 
-
-function insert_user(username, password, sessionID){
-  account_select_username.all([username] , (err, rows) => {
-    if (err) throw err;
-    if(rows.length == 0)
-      bcrypt.hash(password, 10, function(err, hash) {
-        account_insert.run([username, hash, sessionID]);
-      });
-  });
-}
-
-function insert_post(message, req, res){
-  account_select_session.get([req.sessionID], (err, row) => {
-    if (err) throw err;
-    forum_insert_post.run([message, row['username']], () => {
-      render_forum('pages/forum', req, res);
-    });
-  });
-}
-
-function check_login(username, password, req, res){
-  account_select_username.each([username] , (err, row) => {
-    if (err) throw err; 
-    if (bcrypt.compareSync(password, row['password']))  {
-      account_insert.run(row['username'],row['password'],req.sessionID);
-      res.render('pages/home', { welcome_name: username, logged_in: true  });
-      return;
-    }
-    res.render('pages/login', { error_msg: "Invalid Login Details" })
-  });
-}
-
 function create_user(username, password, password2, req, res){ 
   if (password === password2)  {
     insert_user(username, password, req.sessionID);
@@ -144,9 +118,49 @@ function create_user(username, password, password2, req, res){
   }
 }
 
+function insert_user(username, password, sessionID){
+  account_select_username.all([username] , (err, rows) => {
+    if (err) throw_err(err);
+    if(rows.length == 0)
+      bcrypt.hash(password, 10, function(err, hash) {
+        account_insert.run([username, hash, sessionID]);
+      });
+  });
+}
+
+function insert_post(message, req, res){
+  account_select_session.get([req.sessionID], (err, row) => {
+    if (err) throw_err(err);
+    forum_insert_post.run([message, row['username']], () => {
+      render_forum('pages/forum', req, res);
+    });
+  });
+}
+
+function insert_reply(post_id, reply, req, res){
+  account_select_session.get([req.sessionID], (err, row) => {
+    if (err) throw_err(err);
+    replies_insert_reply.run([post_id, reply, row['username']], () => {
+      render_forum('pages/forum', req, res);
+    });
+  });
+}
+
+function check_login(username, password, req, res){
+  account_select_username.each([username] , (err, row) => {
+    if (err) throw_err(err);
+    if (bcrypt.compareSync(password, row['password']))  {
+      account_insert.run(row['username'],row['password'],req.sessionID);
+      res.render('pages/home', { welcome_name: username, logged_in: true  });
+      return;
+    }
+    res.render('pages/login', { error_msg: "Invalid Login Details" })
+  });
+}
+
 function existing_session(view, req, res, args){
   account_select_session.all([req.sessionID] , (err, rows) => {
-    if (err) throw err;
+    if (err) throw_err(err);
     if(rows.length == 0)
       res.render(view, Object.assign({}, { welcome_name: 'there' }, args));
     else
@@ -156,16 +170,16 @@ function existing_session(view, req, res, args){
 
 function render_forum(view, req, res)  {
   forum_select.all( (err, rows) => {
-    if (err) throw err;
+    if (err) throw_err(err);
     var forum_list=[];
     for (var r=0; r < rows.length; r++)  {
-      var post = { username: rows[r]['username'], message: rows[r]['message'], replies: [] };
+      var post = { post_id: rows[r]['post_id'].toString(), username: rows[r]['username'], message: rows[r]['message'], replies: [] };
       forum_list.push(post);
     }
     replies_select.all( (err, rows) => {
       for (var r=0; r < rows.length; r++)  {
         var reply = { username: rows[r]['username'], reply: rows[r]['message']};
-        forum_list[rows[r]['post_id']].replies.push(reply);
+        forum_list[rows[r]['post_id']-1].replies.push(reply);
       }
       existing_session(view, req, res, { posts : forum_list.reverse()});
     });
@@ -175,8 +189,13 @@ function render_forum(view, req, res)  {
 
 function logout(req, res)  {
   account_select_session.get([req.sessionID] , (err, row) => {
-    if (err) throw err;
+    if (err) throw_error(err);
     account_logout.run(row['username'],row['password']);
     res.render('pages/home', { welcome_name: 'there'});
   });
 } 
+
+function throw_error(err)  {
+  console.log(err);
+  existing_session('pages/tutorial', req, res, {});
+}
