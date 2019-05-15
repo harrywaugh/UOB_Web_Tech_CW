95fa930c53d289a6c35186db6dee79ed3b58f12d
Harry Waugh <hw16471@my.bristol.ac.uk>
2019-05-15 17:35:15 +0100

    Click post to center on screen

--- site/views/js/forum.js
+++ site/views/js/forum.js
@@ -1,10 +1,31 @@
 
-$(document).ready(hover_over);
+$(document).ready(hover_over_postbox);
 
-function hover_over() {
-	$('.postbox').hover(
-       function(){ $(this).addClass('color_border') },
-       function(){ $(this).removeClass('color_border') }
-	);
+function hover_over_postbox() {
+    $('.postbox').hover(
+       function(){ $(this).addClass('color_border'); },
+       function(){ $(this).removeClass('color_border'); }
+    );
 }
 
+
+$(document).ready(click_on_postbox);
+
+function click_on_postbox() {
+	$('.postbox').click(
+		function(){ 
+	   	$(this).addClass('abs_center'); 
+	   	$('.background_shader').css('z-index', 1000); 
+	});
+}
+
+
+$(document).ready(click_off_postbox);
+
+function click_off_postbox() {
+	$('.background_shader').click(
+		function(){ 
+	   	$('.postbox').removeClass('abs_center'); 
+	   	$(this).css('z-index', -10000); 
+	});
+}
